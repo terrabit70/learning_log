5a45860b03fe730654fff31516b938e4c88433c1
terrabit70 <30767672+terrabit70@users.noreply.github.com>
2019-12-04 16:08:10 +0700

    Function for adding new topics added

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # Homepage
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
-    path('topics/<topic_id>/', views.topic, name='topic')
+    path('topics/<topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Topic
+from .forms import TopicForm
 # Create your views here.
 def index(request):
     """Homepage of learning log application"""
@@ -17,4 +20,17 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Creates a new topic"""
+    if request.method != 'POST':
+        form = TopicForm
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    context = {'form': form}
+    return render(request=request, template_name='learning_logs/new_topic.html', context=context)
+
 
