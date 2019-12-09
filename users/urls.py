a05b023875a8b258e658228a9e5cfdd1a990dbd1
terrabit70 <30767672+terrabit70@users.noreply.github.com>
2019-12-09 20:21:45 +0700

    Registration added

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def logout_view(request):
     logout(request=request)
     return HttpResponseRedirect(reverse('learning_logs:index'))
+
+def register(request):
+    """Registration of new user"""
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request=request, user=authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+
+    context = {'form': form}
+    return render(request=request, template_name='users/register.html', context=context)
