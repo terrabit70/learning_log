81474bce2293b98f720833a4ff9cfa6eb0147986
terrabit70 <30767672+terrabit70@users.noreply.github.com>
2019-12-09 19:47:09 +0700

    Logout fuction added

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,10 @@
 
 from django.urls import path
 from django.contrib.auth.views import LoginView
+from . import views
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import logout
 
 # Create your views here.
+def logout_view(request):
+    logout(request=request)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
