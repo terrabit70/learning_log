682d68eca769a90463caa58921527f7a8c83bee5
terrabit70 <30767672+terrabit70@users.noreply.github.com>
2020-01-09 15:30:20 +0700

    Separation of topics for users added

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,20 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Topic(models.Model):
     """Topic, wich user learning"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+
     def __str__(self):
         return self.text
+
 
 class Entry(models.Model):
     """Information, which user has learned by the topic"""
     topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         verbose_name_plural = 'entries'
+
     def __str__(self):
         if len(self.text) > 49:
             return self.text[:50] + "..."
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
     """Homepage of learning log application"""
@@ -12,19 +14,24 @@ def about(request):
     """Page with information about project and author"""
     return render(request, 'learning_logs/about.html')
 
+@login_required
 def topics(request):
     """Shows list of topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Shows the theme and its topics"""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Creates a new topic"""
     if request.method != 'POST':
@@ -32,11 +39,14 @@ def new_topic(request):
     else:
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     context = {'form': form}
     return render(request=request, template_name='learning_logs/new_topic.html', context=context)
 
+@login_required
 def new_entry(request, topic_id):
     """Adds a new entry for the topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -52,6 +62,22 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request=request, template_name='learning_logs/new_entry.html', context=context)
 
+@login_required
+def edit_entry(request, entry_id):
+    """Edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse(viewname='learning_logs:topic', args=[topic.id]))
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request=request, template_name='learning_logs/edit_entry.html', context=context)
 
 
 
