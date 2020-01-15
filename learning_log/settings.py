2041573eefbe252081c14c9aa1ab3f22ef7c9345
terrabit70 <ivgen_hunt@mail.ru>
2020-01-15 16:48:18 +0700

    qwe

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -147,6 +147,4 @@ if os.getcwd() == '/app':
     BASE_DIR = os.path.dirname(os.path.dirname(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATIC_URL = '/static/'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+   # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
