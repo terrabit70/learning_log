a24bb3c4242ffc481b9fe55dc8e8a7e1b43c4b7f
terrabit70 <ivgen_hunt@mail.ru>
2020-01-15 16:22:25 +0700

    qwe

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -144,7 +144,7 @@ if os.getcwd() == '/app':
     ALLOWED_HOSTS = ['*']
 
     #Конфигурация статических ресурсов
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
     STATICFILES_DIRS = (
