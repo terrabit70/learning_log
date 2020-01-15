8dee1493872013585338f732c9a643b0b912cdf8
terrabit70 <30767672+terrabit70@users.noreply.github.com>
2020-01-15 15:02:07 +0700

    ready to deploy

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '04*9udhe52072vopnjfh+f%f8v#zj3nt3ufn6-)_g#e=7^@j30'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
